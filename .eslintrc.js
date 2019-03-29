f251f1d1be27a843a59d640e7834afa6ccdcbd0f
Sándor Levcsák <levchak.sanek@gmail.com>
2019-03-29 22:55:29 +0100

    feat(app) Game logic is ready

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,31 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
-  extends: ["plugin:vue/recommended"],
+
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    indent: ["error", 2],
+    indent: ["warn", 2],
     "comma-dangle": ["error", "always-multiline"],
+    quotes: ["error", "double"],
+    "max-len": ["error", 150],
+    "consistent-return": "off",
+    eqeqeq: "off",
+    "no-alert": "off",
+    "no-continue": "off",
+    "import/extensions": "off",
   },
+
   parserOptions: {
     parser: "babel-eslint",
   },
+
+  extends: [
+    "plugin:vue/recommended",
+    "plugin:vue/essential",
+    "@vue/airbnb",
+  ],
 };
