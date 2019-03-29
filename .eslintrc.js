2cbb1629848e069b488ef1c0eba3cd915491ce7f
Sándor Levcsák <levchak.sanek@gmail.com>
2019-03-29 15:05:46 +0100

    fail

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
   },
   extends: ["plugin:vue/recommended"],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-console": "error",
+    // "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     indent: ["error", 2],
     "comma-dangle": ["error", "always-multiline"],
--- src/App.vue
+++ src/App.vue
@@ -16,5 +16,8 @@ export default {
     Header,
     Footer,
   },
+  mounted() {
+    console.log("a");
+  }
 };
 </script>
